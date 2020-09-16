249de9c1fd61316f8ac03e17d9cc408278bd54f8
Vlad <nv.novikovvlad@gmail.com>
2020-09-16 22:24:38 +0300

    Updates

---
+++ precache-manifest.d535b9565c181f012d1af460f4acac48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a925ae0c03562797213b6361d14a8f7b",
+    "url": "/react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "3df6cfe5c33efeb95a3f",
+    "url": "/react-hw-04-movies/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d9395cb34f2295df8293",
+    "url": "/react-hw-04-movies/static/js/2.0699ea0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-04-movies/static/js/2.0699ea0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df6cfe5c33efeb95a3f",
+    "url": "/react-hw-04-movies/static/js/main.936ac250.chunk.js"
+  },
+  {
+    "revision": "e2395479082b559ca45d",
+    "url": "/react-hw-04-movies/static/js/runtime-main.16c3efb3.js"
+  }
+]);
